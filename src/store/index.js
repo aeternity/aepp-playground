7662edbce786387694cf301fd8aaedce6d140a60
Sadi Qevani <inaveq@gmail.com>
2019-02-07 13:55:41 +0100

    adding vuex files

---
+++ src/store/index.js
@@ -0,0 +1,39 @@
+/**
+ * Vue
+ */
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+/**
+ * Vuex Plugins
+ */
+import createPersistedState from 'vuex-persistedstate'
+
+/**
+ * Vuex
+ */
+import state from './state'
+import modules from './modules'
+import mutations from './mutations'
+import actions from './actions'
+import getters from './getters'
+
+/**
+ * Installing Vue Plugins
+ */
+Vue.use(Vuex)
+
+/**
+ * Exporting Vuex.Store Instance
+ */
+export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
+  plugins: [createPersistedState({
+    storage: window.sessionStorage
+  })],
+  state,
+  mutations,
+  actions,
+  getters,
+  modules
+})
