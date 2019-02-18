fff94f5d8af15442abb87324d6921d10e5e54311
Sadi Qevani <inaveq@gmail.com>
2019-02-18 15:03:47 +0100

    adding example app

---
+++ examples/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <example />
+  </div>
+</template>
+<script>
+import Example from './components/example.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Example
+  }
+}
+</script>
+<style></style>
