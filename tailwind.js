0d142a3cd556b619012e2fb5a05f5f2e4e9598ad
Sadi Qevani <inaveq@gmail.com>
2019-02-20 15:40:12 +0100

    tailwind add css colors

--- tailwind.js
+++ tailwind.js
@@ -44,89 +44,72 @@ let defaultConfig = require('tailwindcss/defaultConfig')()
 */
 
 let colors = {
+  /**
+   * Custom properties
+   */
   'transparent': 'transparent',
 
-  'black': '#22292f',
-  'grey-darkest': '#3d4852',
-  'grey-darker': '#606f7b',
-  'grey-dark': '#8795a1',
-  'grey': '#b8c2cc',
-  'grey-light': '#dae1e7',
-  'grey-lighter': '#f1f5f8',
-  'grey-lightest': '#f8fafc',
-  'white': '#ffffff',
+  /**
+   * General Palette colors
+   */
+  'black'  : '#000000',
+  'white'  : '#ffffff',
+  'magenta': '#B300FF',
+  'red'    : '#FF0D6A',
+  'cyan'   : '#00E8CC',
 
-  'red-darkest': '#3b0d0c',
-  'red-darker': '#621b18',
-  'red-dark': '#cc1f1a',
-  'red': '#e3342f',
-  'red-light': '#ef5753',
-  'red-lighter': '#f9acaa',
-  'red-lightest': '#fcebea',
+  /**
+   * Neutral Palette colors
+   */
+  'neutral-minimum'   : '#001833',
+  'neutral-negative-3': '#203040',
+  'neutral-negative-2': '#4E5A66',
+  'neutral-negative-1': '#76818C',
+  'neutral'           : '#929CA6',
+  'neutral-positive-1': '#D3DCE6',
+  'neutral-positive-2': '#EDF3F7',
+  'neutral-positive-3': '#F7FAFC',
+  'neutral-maximum'   : '#FFFFFF',
 
-  'orange-darkest': '#462a16',
-  'orange-darker': '#613b1f',
-  'orange-dark': '#de751f',
-  'orange': '#f6993f',
-  'orange-light': '#faad63',
-  'orange-lighter': '#fcd9b6',
-  'orange-lightest': '#fff5eb',
+  /**
+   * Primary Palette colors
+   */
+  'primary-negative-3': '#BD2A67',
+  'primary-negative-2': '#D12869',
+  'primary-negative-1': '#E72B6E',
+  'primary'           : '#FF0D6A',
+  'primary-positive-1': '#F279A8',
+  'primary-positive-2': '#FA96B7',
+  'primary-positive-3': '#FFBFD7',
 
-  'yellow-darkest': '#453411',
-  'yellow-darker': '#684f1d',
-  'yellow-dark': '#f2d024',
-  'yellow': '#ffed4a',
-  'yellow-light': '#fff382',
-  'yellow-lighter': '#fff9c2',
-  'yellow-lightest': '#fcfbeb',
+  /**
+   * Secondary Palette colors
+   */
+  'secondary-negative-3': '#442873',
+  'secondary-negative-2': '#513482',
+  'secondary-negative-1': '#5F4191',
+  'secondary'           : '#6948A1',
+  'secondary-positive-1': '#8974B3',
+  'secondary-positive-2': '#A18FBF',
+  'secondary-positive-3': '#B2A3CC',
 
-  'green-darkest': '#0f2f21',
-  'green-darker': '#1a4731',
-  'green-dark': '#1f9d55',
-  'green': '#38c172',
-  'green-light': '#51d88a',
-  'green-lighter': '#a2f5bf',
-  'green-lightest': '#e3fcec',
+  /**
+   * Alternative Palette colors
+   */
+  'alternative-negative-3': '#099C9D',
+  'alternative-negative-2': '#05ADA8',
+  'alternative-negative-1': '#02BDAA',
+  'alternative'           : '#14CCB7',
+  'alternative-positive-1': '#62D9CB',
+  'alternative-positive-2': '#95E6DC',
+  'alternative-positive-3': '#CEF2EE',
 
-  'teal-darkest': '#0d3331',
-  'teal-darker': '#20504f',
-  'teal-dark': '#38a89d',
-  'teal': '#4dc0b5',
-  'teal-light': '#64d5ca',
-  'teal-lighter': '#a0f0ed',
-  'teal-lightest': '#e8fffe',
-
-  'blue-darkest': '#12283a',
-  'blue-darker': '#1c3d5a',
-  'blue-dark': '#2779bd',
-  'blue': '#3490dc',
-  'blue-light': '#6cb2eb',
-  'blue-lighter': '#bcdefa',
-  'blue-lightest': '#eff8ff',
-
-  'indigo-darkest': '#191e38',
-  'indigo-darker': '#2f365f',
-  'indigo-dark': '#5661b3',
-  'indigo': '#6574cd',
-  'indigo-light': '#7886d7',
-  'indigo-lighter': '#b2b7ff',
-  'indigo-lightest': '#e6e8ff',
-
-  'purple-darkest': '#21183c',
-  'purple-darker': '#382b5f',
-  'purple-dark': '#794acf',
-  'purple': '#9561e2',
-  'purple-light': '#a779e9',
-  'purple-lighter': '#d6bbfc',
-  'purple-lightest': '#f3ebff',
-
-  'pink-darkest': '#451225',
-  'pink-darker': '#6f213f',
-  'pink-dark': '#eb5286',
-  'pink': '#f66d9b',
-  'pink-light': '#fa7ea8',
-  'pink-lighter': '#ffbbca',
-  'pink-lightest': '#ffebef',
+  /**
+   * Utilities Palette colors
+   */
+  'error':     '#FF0D6A',
+  'focus':     '#B300FF',
+  'focus-alt': '#00E8CC'
 }
 
 module.exports = {
@@ -408,8 +391,8 @@ module.exports = {
     default: '1px',
     '0': '0',
     '2': '2px',
+    '3': '3px',
     '4': '4px',
-    '8': '8px',
   },
 
 
@@ -977,6 +960,5 @@ module.exports = {
     prefix: '',
     important: false,
     separator: ':',
-  },
-
+  }
 }
