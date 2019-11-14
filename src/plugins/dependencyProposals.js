ccfc7fcb483ce7af8d7a4f2e8855a38cce574a32
HristiyanG <50369418+HristiyanG@users.noreply.github.com>
2019-11-14 17:35:44 +0200

    Feature/define sophia language syntax (#157)
    * changed identity to cryptohamster
    
    * cryptohamster
    
    * refactor smart contract
    
    * changed places of writing to state and emitting event
    
    * added some new types to suppor newest version of sophia language
    
    * added auto closing brackets
    
    * added auto closing brackets
    
    * added :: operator, removed private/public keywords
    
    * added private as it is still neccessary
    
    * changed foreground color for highlighting and rows counter
    
    * added new auto-complete functionality
    
    * fixed autocomplete suggestions
    
    * revised keywords and updated sdk version to latest
    
    * added omitted keywords
    
    * removed unneccessary keywords
    
    * changed keywords

---
+++ src/plugins/dependencyProposals.js
@@ -0,0 +1,117 @@
+import * as monaco from "monaco-editor"
+
+const sophiaKeyWords = [
+    'require',
+    'abort',
+    'payable',
+    'contract',
+    'function',
+    'entrypoint',
+    'address',
+    'string',
+    'bytes',
+    'bool',
+    'list',
+    'tuple',
+    'record',
+    'Bits',
+    'bits',
+    'event',
+    'enum',
+    'int',
+    'state',
+    'signature',
+    'let',
+    'mod',
+    'stateful',
+    'namespace',
+    'map',
+    'Map',
+    'hash',
+    'String',
+    'public',
+    'private',
+    'payable',
+    'true',
+    'false',
+    'include',
+    'switch',
+    'if',
+    'elif',
+    'else',
+    'delete',
+    'type',
+    'datatype',
+    'put',
+    'Call',
+    'caller',
+    'value',
+    'Chain',
+    'Oracle',
+    'oracle',
+    'oracle_query',
+    'Crypto',
+    'AENS',
+    'resolve',
+    'preclaim',
+    'claim',
+    'transfer',
+    'revoke',
+    'Contract',
+    'creator',
+    'address',
+    'balance'
+    ]
+
+function getReservedWordsFromContract(contractContent) {
+    let contractKeyWords = ['entrypoint', 'function', 'contract']
+    let reserved = []
+
+    for (const key in contractKeyWords) {
+        let contractKeywordString = `(?:\\b${contractKeyWords[key]}\\s+(\\w+\\'*))`
+        let regex = new RegExp(contractKeywordString, "gim")
+
+        let result
+
+        while ((result = regex.exec(contractContent)) !== null) {
+            reserved.push(result[1])
+        }
+    }
+
+    return reserved
+}
+
+function getCombinedKeyWords(contractKeyWordsArr) {
+    let sophiaKW = sophiaKeyWords.slice(0)
+    let map = new Map()
+    let combinedKeyWords = []
+
+    for (let kw of sophiaKW) {
+        map.set(kw, true)
+        combinedKeyWords.push({
+            label: kw,
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            insertText: kw
+        })
+    }
+
+    for (let extractedWord of contractKeyWordsArr) {
+        if (!map.has(extractedWord)) {
+            map.set(extractedWord, true)
+            combinedKeyWords.push({
+                label: extractedWord,
+                kind: monaco.languages.CompletionItemKind.Keyword,
+                insertText: extractedWord
+            })
+        }
+    }
+
+    return combinedKeyWords
+}
+
+export function createDependencyProposals(contractContent) {
+    let contractKeyWordsArr = getReservedWordsFromContract(contractContent)
+    let combinedKeyWords = getCombinedKeyWords(contractKeyWordsArr)
+    
+    return combinedKeyWords
+}
